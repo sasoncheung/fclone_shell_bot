fa9f6d3c484672b5d06172e8c8b454f8892d6e40
sasoncheung <36258465+sasoncheung@users.noreply.github.com>
2020-08-27 22:00:49 +0800

    Update fbcopy.sh

--- script/fbcopy.sh
+++ script/fbcopy.sh
@@ -1,37 +1,115 @@
 #!/bin/bash
 #=============================================================
-# https://github.com/cgkings/fcp
-# File Name: fbtask.sh
+# https://github.com/cgkings/fcs
+# File Name: fsize.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:全盘备份-task
+# Description:size查询
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
-source /root/fcp/myfc_config.ini
+source /root/fcs/myfc_config.ini
 
-clear
-echo -e " 选择你需要备份的盘
-[1]. 1#盘
-[2]. 2#盘
-[3]. 3#盘
-[4]. 退出"
-read -p "请输入数字 [1-5]:" num
+#三种模式，在myfc_config.ini中选择size_mode
+#其中：1#，ls基础模式，2#，ls列表模式，3#，size基础模式,3#size列表模式
+read -p "请输入查询链接==>" link
+link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
+rootname=$(fclone lsd "$fclone_name3":{$link} --disable listR --dump bodies -vv 2>&1 | awk 'BEGIN{FS="\""}/^{"id/{print $8}')
+if [ -z "$link" ] ; then
+echo "不允许输入为空" && exit ;
+fi
+#1号，ls基础模式
+size_mode_num_simple() {
+    file_num=$(fclone ls "$fclone_name3":{$link} --disable listR --checkers="$fs_chercker" | wc -l)
+    folder_num=$(fclone lsd "$fclone_name3":{$link} --disable listR -R --checkers="$fs_chercker" | wc -l)
+    echo -e "▣▣▣▣▣▣▣▣查询信息▣▣▣▣▣▣▣▣\n" 
+    echo -e "┋ name  ┋:$rootname \n"
+    echo -e "┋ file  ┋:$file_num \n"
+    echo -e "┋ folder┋:$folder_num \n"
+    echo -e "┋ total ┋:$[file_num+folder_num] \n"
+}
+#2号，ls列表模式
+size_mode_num() {
+    file_num0=$(fclone ls "$fclone_name3":{$link} --disable listR --checkers="$fs_chercker" | wc -l)
+    file_num1=$(fclone ls "$fclone_name3":{$link} --include "*.{avi,mpeg,wmv,mp4,mkv,rm,rmvb,3gp,mov,flv,vob}" --ignore-case --disable listR --checkers="$fs_chercker" | wc -l)
+    file_num2=$(fclone ls "$fclone_name3":{$link} --include "*.{png,jpg,jpeg,gif,webp,tif}" --ignore-case --disable listR --checkers="$fs_chercker" | wc -l)
+    file_num3=$(fclone ls "$fclone_name3":{$link} --include "*.{html,htm,txt,pdf,nfo}" --ignore-case --disable listR --checkers="$fs_chercker" | wc -l)
+    echo -e "资源名称："$rootname""
+    echo -e "--------------"
+    printf "|%-5s|%-8s|\n" 类型 文件数量
+    echo -e "--------------"
+    printf "|%-5s|%-8s|\n" 视频 "$file_num1"
+    echo -e "--------------"
+    printf "|%-5s|%-8s|\n" 图片 "$file_num2"
+    echo -e "--------------"
+    printf "|%-5s|%-8s|\n" 文本 "$file_num3"
+    echo -e "--------------"
+    printf "|%-5s|%-8s|\n" 合计 "$file_num0"
+    echo -e "--------------"
+}
+#3号，size基础模式
+size_mode_simple() {
+    size_info=`fclone size "$fclone_name3":{$link} --disable listR --checkers="$fs_chercker"`
+    file_num=$(echo "$size_info" | awk 'BEGIN{FS=" "}/^Total objects/{print $3}')
+    file_size=$(echo "$size_info" | awk 'BEGIN{FS=" "}/^Total size/{print $3,$4}')
+    echo -e "▣▣▣▣▣▣▣▣查询信息▣▣▣▣▣▣▣▣\n" 
+    echo -e "┋资源名称┋:$rootname \n"
+    echo -e "┋资源数量┋:$file_num \n"
+    echo -e "┋资源大小┋:$file_size \n"
+}
+#4号，size列表模式
+size_mode_fully() {
+    size_info0=`fclone size "$fclone_name3":{$link} --disable listR --checkers="$fs_chercker"`
+    file_num0=$(echo "$size_info0" | awk 'BEGIN{FS=" "}/^Total objects/{print $3}')
+    file_size0=$(echo "$size_info0" | awk 'BEGIN{FS=" "}/^Total size/{print $3,$4}')
+    size_info1=`fclone size "$fclone_name3":{$link} --include "*.{avi,mpeg,wmv,mp4,mkv,rm,rmvb,3gp,mov,flv,vob}" --ignore-case --disable listR --checkers="$fs_chercker"`
+    file_num1=$(echo "$size_info1" | awk 'BEGIN{FS=" "}/^Total objects/{print $3}')
+    file_size1=$(echo "$size_info1" | awk 'BEGIN{FS=" "}/^Total size/{print $3,$4}')
+    size_info2=`fclone size "$fclone_name3":{$link} --include "*.{png,jpg,jpeg,gif,webp,tif}" --ignore-case --disable listR --checkers="$fs_chercker"`
+    file_num2=$(echo "$size_info2" | awk 'BEGIN{FS=" "}/^Total objects/{print $3}')
+    file_size2=$(echo "$size_info2" | awk 'BEGIN{FS=" "}/^Total size/{print $3,$4}')
+    size_info3=`fclone size "$fclone_name3":{$link} --include "*.{html,htm,txt,pdf,nfo}" --ignore-case --disable listR --checkers="$fs_chercker"`
+    file_num3=$(echo "$size_info3" | awk 'BEGIN{FS=" "}/^Total objects/{print $3}')
+    file_size3=$(echo "$size_info3" | awk 'BEGIN{FS=" "}/^Total size/{print $3,$4}')
+    echo -e "资源名称："$rootname""
+    echo -e "----------------------------------"
+    printf "|%-5s|%-8s|%-18s|\n" 类型 文件数量 文件大小
+    echo -e "----------------------------------"
+    printf "|%-5s|%-8s|%-18s|\n" 视频 "$file_num1" "$file_size1"
+    echo -e "----------------------------------"
+    printf "|%-5s|%-8s|%-18s|\n" 图片 "$file_num2" "$file_size2"
+    echo -e "----------------------------------"
+    printf "|%-5s|%-8s|%-18s|\n" 文本 "$file_num3" "$file_size3"
+    echo -e "----------------------------------"
+    printf "|%-5s|%-8s|%-18s|\n" 合计 "$file_num0" "$file_size0"
+    echo -e "----------------------------------"
+}
+echo -e " 选择模式
+[1]. ls基础模式
+[2]. ls列表模式
+[3]. size基础模式
+[4]. size列表模式"
+read -p "请输入数字 [1-4]:" num
 case "$num" in
 1)
-    echo -e "★★★ 1#盘 ★★★"
-    myid="$one_id"
+    echo -e "你的选择，ls基础模式"
+    size_mode_num_simple
+    exit
     ;;
 2)
-    echo -e "★★★ 2#盘 ★★★"
-    myid="$two_id"
+    echo -e "你的选择，ls列表模式"
+    size_mode_num
+    exit
     ;;
 3)
-    echo -e "★★★ 3#盘 ★★★"
-    myid="$three_id"
+    echo -e "你的选择，size基础模式"
+    size_mode_simple
+    exit
     ;;
 4)
+    echo -e "你的选择，size列表模式"
+    size_mode_fully
     exit
     ;;
 *)
@@ -39,16 +117,3 @@ case "$num" in
     exit
     ;;
 esac
-read -p "请输入备份到盘ID==>" link
-if [ -z "$link" ] ; then
-echo "不允许输入为空" && exit
-else
-link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
-fi
-echo -e "▣▣▣▣▣▣执行备份▣▣▣▣▣▣"
-fclone copy "$fclone_name":{$myid} "$fclone_name":{$link} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first --ignore-existing --log-level=ERROR --log-file=/root/fcp/log/fbcopy.log
-echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  备份完毕"
-echo -e "▣▣▣▣▣▣执行同步▣▣▣▣▣▣"
-fclone sync "$fclone_name":{$myid} "$fclone_name":{$link} --drive-server-side-across-configs --drive-use-trash=false --stats=1s --stats-one-line -P --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first --log-level=ERROR --log-file=/root/fcp/log/fbsync.log
-echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  同步完毕"
-exit
