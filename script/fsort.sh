c5116ec0cc01def02fab4302f22b0a677dda8be6
sasoncheung <36258465+sasoncheung@users.noreply.github.com>
2020-08-27 18:32:53 +0800

    Update fsort.sh

--- script/fsort.sh
+++ script/fsort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #=============================================================
-# https://github.com/cgkings/fclone_shell_bot
+# https://github.com/cgkings/fcs
 # File Name: fsort.sh
 # Author: cgking
 # Created Time : 2020.7.8
@@ -9,7 +9,7 @@
 # Version: final
 #=============================================================
 
-source /root/fclone_shell_bot/myfc_config.ini
+source /root/fcs/myfc_config.ini
 clear
 read -p "请输入目标链接==>" link1
 link1=${link1#*id=};link1=${link1#*folders/};link1=${link1#*d/};link1=${link1%?usp*}
@@ -20,10 +20,10 @@ for input_id in $input_ids
 do
    input_name=$(echo "$input_info" | grep '"'$input_id'"' | cut  -d '"' -f 8)
    echo "$input_name"
-   output_names=$(cut -d ":" -f 1 /root/fclone_shell_bot/av_num.txt)
+   output_names=$(cut -d ":" -f 1 /root/fcs/av_num.txt)
    if ( $input_names =~ *$output_names* ); then
-   output_id=$(awk 'BEGIN{FS=":"}/^'$output_names'/{print $2}' /root/fclone_shell_bot/av_num.txt)
-   fclone copy "$fclone_name":{$input_id} "$fclone_name":{$output_id} --drive-server-side-across-configs --fast-list --no-traverse --size-only --stats=1s --stats-one-line -P --drive-pacer-min-sleep=1ms --ignore-checksum --ignore-existing --buffer-size=50M --use-mmap --checkers=8 --transfers=8 --check-first --log-level=ERROR --log-file=/root/fclone_shell_bot/log/fsingle.log
+   output_id=$(awk 'BEGIN{FS=":"}/^'$output_names'/{print $2}' /root/fcs/av_num.txt)
+   fclone copy "$fclone_name":{$input_id} "$fclone_name":{$output_id} --drive-server-side-across-configs --fast-list --no-traverse --size-only --stats=1s --stats-one-line -P --drive-pacer-min-sleep=1ms --ignore-checksum --ignore-existing --buffer-size=50M --use-mmap --checkers=8 --transfers=8 --check-first --log-level=ERROR --log-file=/root/fcs/log/fsingle.log
    else
    echo "无可整理的文件"
    fi
